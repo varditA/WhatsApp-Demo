d619f8b37abbc899303a71eb6d5dc0d20e56ff93
Vardit Arkash <rosea2468@gmail.com>
2017-06-21 18:19:35 +0000

    Group.cpp edited online with Bitbucket

--- src/ServerSide/Group.cpp
+++ src/ServerSide/Group.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Group.h"
+
+Group::Group(string groupName, set<ClientInfo*> clientsInGroup) {
+    this->name = groupName;
+    for (ClientInfo * client: clientsInGroup)
+    {
+        client->addGroup(this);
+        clients.insert(client);
+    }
+}
