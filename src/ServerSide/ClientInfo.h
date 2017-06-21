49eb309774e0e4867fca293a1bfbaa5abd31ec60
Vardit Arkash <rosea2468@gmail.com>
2017-06-21 18:18:20 +0000

    ClientInfo.h edited online with Bitbucket

--- src/ServerSide/ClientInfo.h
+++ src/ServerSide/ClientInfo.h
@@ -2,8 +2,46 @@
 // Created by Vardit on 19/06/2017.
 //
 
-#include "ClientInfo.h"
+#ifndef EX5_CLIENTINFO_H
+#define EX5_CLIENTINFO_H
 
-ClientInfo::ClientInfo(int socket, const string name): socketId(socket), name(name) {}
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <vector>
+#include "Group.h"
 
-ClientInfo::~ClientInfo() {}
+class Group;
+using namespace std;
+
+class ClientInfo{
+
+private:
+    int socketId;
+    const string name;
+    vector <Group *> groups;
+
+public:
+    ClientInfo(int socket, const string name);
+    ~ClientInfo();
+
+    inline int getSocketId() const {
+        return socketId;
+    }
+
+    inline const string &getName() const {
+        return name;
+    }
+
+    inline vector<Group*> getGroups() const {
+        return groups;
+    }
+
+    inline void addGroup(Group * group) {
+        this->groups.push_back(group);
+    }
+
+
+};
+
+#endif //EX5_CLIENTINFO_H
